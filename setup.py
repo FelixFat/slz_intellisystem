f7545dbdea1810bc550f8556e02759e2cac60a05
flex <felixfat9@gmail.com>
2023-05-27 15:29:06 +0300

    Update package

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
 package_name = 'slz_intellisystem'
+submodules = f"{package_name}/submodules"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=[package_name],
+    version='0.1.0',
+    packages=[package_name, submodules],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        #('lib/' + package_name, ['./include/model.h5']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'start = slz_intellisystem.slz_intellisystem:main'
+            'start = slz_intellisystem.slz_evaluation:main'
         ],
     },
 )
